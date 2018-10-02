43d326fd1a3cdd0515afd2139b409893ffbaa55d
Sasha <stepowoi.s@gmail.com>
2018-10-02 11:54:21 +0300

    2_5

--- src/Studentlist.java
+++ src/Studentlist.java
@@ -5,11 +5,26 @@ import java.util.List;
 
 class StudentList {
     public List<Student> studentList;
-    public void AddStudent(){
+    public String surename;
+    public int dateofBirth;
+
+    public void AddStudent() {
         Student student = new Student();
         ArrayList studentsList = new ArrayList();
         studentsList.add(student);
 
+    }
+
+    public StudentList(String surename) {
+        this.surename = surename;
+    }
+
+    public String getSurename() {
+        return surename;
+    }
+
+    public int getDateofBirth() {
+        return dateofBirth;
     }
 }
 

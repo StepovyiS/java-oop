75362ed3b3fad55e578fa6d8d431067dda11a95d
Sasha <stepowoi.s@gmail.com>
2018-10-02 10:53:55 +0300

    1

---
+++ src/Students.java
@@ -0,0 +1,9 @@
+public class Students {
+    public String surename;
+    public Students(String surename){
+        this.surename= surename;
+    }
+    public String getSurename(){
+        return surename;
+    }
+}

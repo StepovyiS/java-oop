412a4ca5430d3cac3830a741cdf4a0b80d3a14ee
Sasha <stepowoi.s@gmail.com>
2018-10-07 16:20:07 +0300

    3_1

---
+++ src/developer.java
@@ -0,0 +1,11 @@
+public class developer {
+    protected String name;
+    protected double basicSalary;
+    protected int experience;
+
+    public developer(String name, double basicSalary, int experience) {
+        this.name = name;
+        this.basicSalary = basicSalary;
+        this.experience = experience;
+    }
+}
